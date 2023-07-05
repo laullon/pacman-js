5b9be223dc05688e5e7593c0ca15612e5fd8a5ad
German Laullon <glaullon@vmware.com>
2023-07-05 23:33:20 +0200

    reformat

--- entities.js
+++ entities.js
@@ -1,188 +1,210 @@
 const Direction = {
-    RIGHT: 0,
-    DOWN: 1,
-    LEFT: 2,
-    UP: 3,
-}
+  RIGHT: 0,
+  DOWN: 1,
+  LEFT: 2,
+  UP: 3,
+};
 
 class Character {
-    constructor(x,y, image) {
-        var x, y, image;
-        this.x = x * blockSize;
-        this.y = y * blockSize;
-        this.image = image;
-        this.speed = 1;
-        this.direction = Direction.RIGHT;
-        this.nextDirection = Direction.RIGHT;
-    }
-    
-    draw(ctx){
-        ctx.drawImage(this.image, this.x-2, this.y-2, blockSize+4, blockSize+4)
-    }
-    
-    update() {
-        this.move();
-        this.changeDirecction();
-    }
-    
-    move() {
-        let oldX = this.x;
-        let oldY = this.y;
-        
-        switch(this.direction) {
-            case Direction.DOWN:
-            this.y += this.speed;
-            break;
-            case Direction.UP:
-            this.y -= this.speed;
-            break;
-            case Direction.RIGHT:
-            this.x += this.speed
-            break;
-            case Direction.LEFT:
-            this.x -= this.speed
-            break;
-        }
-        
-        let row = parseInt(this.y / blockSize);
-        let col = parseInt(this.x /blockSize);
-        let colOff = parseInt(this.x % blockSize);
-        let rowOff = parseInt(this.y % blockSize);
-        
-        if(this.direction==Direction.RIGHT){
-            if (colOff!=0) {
-                col++;
-            }
-        }
-        
-        if(this.direction==Direction.DOWN){
-            if (rowOff!=0) {
-                row++;
-            }
-        }
-        
-        if (map[row][col] == 0) {
-            this.x = oldX;
-            this.y = oldY;
-        }
+  constructor(x, y, image) {
+    var x, y, image;
+    this.x = x * blockSize;
+    this.y = y * blockSize;
+    this.image = image;
+    this.speed = 1;
+    this.direction = Direction.RIGHT;
+    this.nextDirection = Direction.RIGHT;
+  }
 
-        if (col<0) {
-            this.x = map[0].length*blockSize;
-        } else if (col>map[0].length) {
-            this.x = -blockSize;
-        }
+  draw(ctx) {
+    ctx.drawImage(
+      this.image,
+      this.x - 2,
+      this.y - 2,
+      blockSize + 4,
+      blockSize + 4
+    );
+  }
+
+  update() {
+    this.move();
+    this.changeDirecction();
+  }
+
+  move() {
+    let oldX = this.x;
+    let oldY = this.y;
+
+    switch (this.direction) {
+      case Direction.DOWN:
+        this.y += this.speed;
+        break;
+      case Direction.UP:
+        this.y -= this.speed;
+        break;
+      case Direction.RIGHT:
+        this.x += this.speed;
+        break;
+      case Direction.LEFT:
+        this.x -= this.speed;
+        break;
     }
-    
-    changeDirecction() {
-        if (this.direction == this.nextDirection) return;
-        let row = parseInt(this.y / blockSize);
-        let col = parseInt(this.x /blockSize);
-        let colOff = parseInt(this.x % blockSize);
-        let rowOff = parseInt(this.y % blockSize);
-        if((colOff==0) && (rowOff==0)) {
-            switch (this.nextDirection) {
-                case Direction.DOWN:
-                row++;
-                break;
-                case Direction.UP:
-                row--;
-                break;
-                case Direction.RIGHT:
-                col++;
-                break;
-                case Direction.LEFT:
-                col--;
-                break;
-            }
-            if (map[row][col] != 0) {
-                this.direction=this.nextDirection;
-            }
-        }        
+
+    let row = parseInt(this.y / blockSize);
+    let col = parseInt(this.x / blockSize);
+    let colOff = parseInt(this.x % blockSize);
+    let rowOff = parseInt(this.y % blockSize);
+
+    if (this.direction == Direction.RIGHT) {
+      if (colOff != 0) {
+        col++;
+      }
     }
+
+    if (this.direction == Direction.DOWN) {
+      if (rowOff != 0) {
+        row++;
+      }
+    }
+
+    if (map[row][col] == 0) {
+      this.x = oldX;
+      this.y = oldY;
+    }
+
+    if (col < 0) {
+      this.x = map[0].length * blockSize;
+    } else if (col > map[0].length) {
+      this.x = -blockSize;
+    }
+  }
+
+  changeDirecction() {
+    if (this.direction == this.nextDirection) return;
+    let row = parseInt(this.y / blockSize);
+    let col = parseInt(this.x / blockSize);
+    let colOff = parseInt(this.x % blockSize);
+    let rowOff = parseInt(this.y % blockSize);
+    if (
+      (colOff == 0 && rowOff == 0) ||
+      (this.direction == Direction.LEFT &&
+        this.nextDirection == Direction.RIGHT) ||
+      (this.direction == Direction.RIGHT &&
+        this.nextDirection == Direction.LEFT) ||
+      (this.direction == Direction.UP &&
+        this.nextDirection == Direction.DOWN) ||
+      (this.direction == Direction.DOWN && this.nextDirection == Direction.UP)
+    ) {
+      switch (this.nextDirection) {
+        case Direction.DOWN:
+          row++;
+          break;
+        case Direction.UP:
+          row--;
+          break;
+        case Direction.RIGHT:
+          col++;
+          break;
+        case Direction.LEFT:
+          col--;
+          break;
+      }
+      if (map[row][col] != 0) {
+        this.direction = this.nextDirection;
+      }
+    }
+  }
 }
 
 class PacMan extends Character {
-    frame = 0;
-    images = [
-        [
-            document.getElementById("pacman-right-1"),
-            document.getElementById("pacman-right-2"),
-            document.getElementById("pacman-right-3")
-        ],
-        [
-            document.getElementById("pacman-down-1"),
-            document.getElementById("pacman-down-2"),
-            document.getElementById("pacman-down-3")
-        ],
-        [
-            document.getElementById("pacman-left-1"),
-            document.getElementById("pacman-left-2"),
-            document.getElementById("pacman-left-3")
-        ],
-        [
-            document.getElementById("pacman-up-1"),
-            document.getElementById("pacman-up-2"),
-            document.getElementById("pacman-up-3")
-        ],
-    ];
-    
-    constructor() {
-        super(13, 23, null);
-        this.speed = 2;
-        this.update();
-    }
-    
-    checkKey(event) {
-        var code = event.code;
-        switch (code) {
-            case "ArrowUp":
-            this.nextDirection = Direction.UP;
-            break;
-            case "ArrowDown":
-            this.nextDirection = Direction.DOWN;
-            break;
-            case "ArrowLeft":
-            this.nextDirection = Direction.LEFT;
-            break;
-            case "ArrowRight":
-            this.nextDirection = Direction.RIGHT;
-            break;
-        }
-    }
+  frame = 0;
+  images = [
+    [
+      document.getElementById("pacman-right-1"),
+      document.getElementById("pacman-right-2"),
+      document.getElementById("pacman-right-3"),
+    ],
+    [
+      document.getElementById("pacman-down-1"),
+      document.getElementById("pacman-down-2"),
+      document.getElementById("pacman-down-3"),
+    ],
+    [
+      document.getElementById("pacman-left-1"),
+      document.getElementById("pacman-left-2"),
+      document.getElementById("pacman-left-3"),
+    ],
+    [
+      document.getElementById("pacman-up-1"),
+      document.getElementById("pacman-up-2"),
+      document.getElementById("pacman-up-3"),
+    ],
+  ];
 
-    clearKey(){
-        this.nextDirection = this.direction;
+  constructor() {
+    super(13, 23, null);
+    this.speed = 2;
+    this.update();
+  }
+
+  checkKey(event) {
+    var code = event.code;
+    switch (code) {
+      case "ArrowUp":
+        this.nextDirection = Direction.UP;
+        break;
+      case "ArrowDown":
+        this.nextDirection = Direction.DOWN;
+        break;
+      case "ArrowLeft":
+        this.nextDirection = Direction.LEFT;
+        break;
+      case "ArrowRight":
+        this.nextDirection = Direction.RIGHT;
+        break;
     }
-    
-    update() {
-        super.update()
-        this.frame +=1;
-        this.frame %=3;
-        this.image = this.images[this.direction][this.frame];
-    }
+  }
+
+  clearKey() {
+    this.nextDirection = this.direction;
+  }
+
+  update() {
+    super.update();
+    this.frame += 1;
+    this.frame %= 3;
+    this.image = this.images[this.direction][this.frame];
+  }
+
+  getPosition() {
+    return [
+      parseInt((this.y + blockSize / 2) / blockSize),
+      parseInt((this.x + blockSize / 2) / blockSize),
+    ];
+  }
 }
 
 class Ghost extends Character {
-    changeDirecction() {
-        super.changeDirecction();
-        this.nextDirection = Math.floor(Math.random() * 4);
-    }
+  changeDirecction() {
+    super.changeDirecction();
+    this.nextDirection = Math.floor(Math.random() * 4);
+  }
 }
 
 class Clyde extends Ghost {
-    constructor() {
-        super(11, 14, document.getElementById("ghosts-clyde"));
-    }    
+  constructor() {
+    super(11, 14, document.getElementById("ghosts-clyde"));
+  }
 }
 
 class Inky extends Ghost {
-    constructor() {
-        super(13, 14, document.getElementById("ghosts-inky"));
-    }    
+  constructor() {
+    super(13, 14, document.getElementById("ghosts-inky"));
+  }
 }
 
 class Pinky extends Ghost {
-    constructor() {
-        super(15, 14, document.getElementById("ghosts-pinky"));
-    }    
+  constructor() {
+    super(15, 14, document.getElementById("ghosts-pinky"));
+  }
 }
--- game.js
+++ game.js
@@ -1,152 +1,296 @@
-/*
-* 0 = wall
-* 1 = dot
-* 2 = power up
-* 3 = empty
-* 4 = door
-*/
+const Map = {
+  WALL: 0,
+  DOT: 1,
+  POWER: 2,
+  EMPTY: 3,
+  DOOR: 3,
+};
+
 const map = [
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0],
-    [0,1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1,0],
-    [0,2,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,2,0],
-    [0,1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1,0],
-    [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
-    [0,1,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,1,0],
-    [0,1,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,1,0],
-    [0,1,1,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,1,1,0],
-    [0,0,0,0,0,0,1,0,0,0,0,0,3,0,0,3,0,0,0,0,0,1,0,0,0,0,0,0],
-    [3,3,3,3,3,0,1,0,0,0,0,0,3,0,0,3,0,0,0,0,0,1,0,3,3,3,3,3],
-    [3,3,3,3,3,0,1,0,0,3,3,3,3,3,3,3,3,3,3,0,0,1,0,3,3,3,3,3],
-    [3,3,3,3,3,0,1,0,0,3,0,0,0,4,4,0,0,0,3,0,0,1,0,3,3,3,3,3],
-    [0,0,0,0,0,0,1,0,0,3,0,3,3,3,3,3,3,0,3,0,0,1,0,0,0,0,0,0],
-    [3,3,3,3,3,3,1,3,3,3,0,3,3,3,3,3,3,0,3,3,3,1,3,3,3,3,3,3],
-    [0,0,0,0,0,0,1,0,0,3,0,3,3,3,3,3,3,0,3,0,0,1,0,0,0,0,0,0],
-    [3,3,3,3,3,0,1,0,0,3,0,0,0,0,0,0,0,0,3,0,0,1,0,3,3,3,3,3],
-    [3,3,3,3,3,0,1,0,0,3,3,3,3,3,3,3,3,3,3,0,0,1,0,3,3,3,3,3],
-    [3,3,3,3,3,0,1,0,0,3,0,0,0,0,0,0,0,0,3,0,0,1,0,3,3,3,3,3],
-    [0,0,0,0,0,0,1,0,0,3,0,0,0,0,0,0,0,0,3,0,0,1,0,0,0,0,0,0],
-    [0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0],
-    [0,1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1,0],
-    [0,1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1,0,0,0,0,1,0],
-    [0,2,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,1,1,2,0],
-    [0,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0,0],
-    [0,0,0,1,0,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,1,0,0,0],
-    [0,1,1,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,0,0,1,1,1,1,1,1,0],
-    [0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0],
-    [0,1,0,0,0,0,0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0],
-    [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-]
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0,
+  ],
+  [
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 0,
+  ],
+  [
+    0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+    0, 1, 0,
+  ],
+  [
+    0, 2, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+    0, 2, 0,
+  ],
+  [
+    0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+    0, 1, 0,
+  ],
+  [
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 0,
+  ],
+  [
+    0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0,
+    0, 1, 0,
+  ],
+  [
+    0, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0,
+    0, 1, 0,
+  ],
+  [
+    0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1,
+    1, 1, 0,
+  ],
+  [
+    0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+    0, 0, 0,
+  ],
+  [
+    3, 3, 3, 3, 3, 0, 1, 0, 0, 0, 0, 0, 3, 0, 0, 3, 0, 0, 0, 0, 0, 1, 0, 3, 3,
+    3, 3, 3,
+  ],
+  [
+    3, 3, 3, 3, 3, 0, 1, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 1, 0, 3, 3,
+    3, 3, 3,
+  ],
+  [
+    3, 3, 3, 3, 3, 0, 1, 0, 0, 3, 0, 0, 0, 4, 4, 0, 0, 0, 3, 0, 0, 1, 0, 3, 3,
+    3, 3, 3,
+  ],
+  [
+    0, 0, 0, 0, 0, 0, 1, 0, 0, 3, 0, 3, 3, 3, 3, 3, 3, 0, 3, 0, 0, 1, 0, 0, 0,
+    0, 0, 0,
+  ],
+  [
+    3, 3, 3, 3, 3, 3, 1, 3, 3, 3, 0, 3, 3, 3, 3, 3, 3, 0, 3, 3, 3, 1, 3, 3, 3,
+    3, 3, 3,
+  ],
+  [
+    0, 0, 0, 0, 0, 0, 1, 0, 0, 3, 0, 3, 3, 3, 3, 3, 3, 0, 3, 0, 0, 1, 0, 0, 0,
+    0, 0, 0,
+  ],
+  [
+    3, 3, 3, 3, 3, 0, 1, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 1, 0, 3, 3,
+    3, 3, 3,
+  ],
+  [
+    3, 3, 3, 3, 3, 0, 1, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 1, 0, 3, 3,
+    3, 3, 3,
+  ],
+  [
+    3, 3, 3, 3, 3, 0, 1, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 1, 0, 3, 3,
+    3, 3, 3,
+  ],
+  [
+    0, 0, 0, 0, 0, 0, 1, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 1, 0, 0, 0,
+    0, 0, 0,
+  ],
+  [
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 0,
+  ],
+  [
+    0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+    0, 1, 0,
+  ],
+  [
+    0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+    0, 1, 0,
+  ],
+  [
+    0, 2, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1,
+    1, 2, 0,
+  ],
+  [
+    0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1,
+    0, 0, 0,
+  ],
+  [
+    0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1,
+    0, 0, 0,
+  ],
+  [
+    0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1,
+    1, 1, 0,
+  ],
+  [
+    0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 0,
+  ],
+  [
+    0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 0,
+  ],
+  [
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 0,
+  ],
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0,
+  ],
+];
+
+var pendingDots;
+var points;
 
 const blockSize = 20; // 800/23
 
-const entities = [new PacMan(), new Clyde(), new Inky(), new Pinky()];
-const player = entities[0];
+const player = new PacMan();
+const entities = [new Clyde(), new Inky(), new Pinky(), player];
 
 function gameLoop() {
-    
-    update();
-    draw();
+  update();
+  checkEndGame();
+  draw();
 }
 
-function draw(){
-    var canvas = document.getElementById("screen");
-    var ctx = canvas.getContext("2d");
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawMap(ctx);
-    drawEntities(ctx);
+function draw() {
+  var canvas = document.getElementById("screen");
+  var ctx = canvas.getContext("2d");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawMap(ctx);
+  drawEntities(ctx);
 }
 
-function update(){
-    entities.forEach(element => {
-        element.update();
-    });
+function update() {
+  entities.forEach((element) => {
+    element.update();
+  });
 }
 
 function drawEntities(ctx) {
-    entities.forEach(element => {
-        element.draw(ctx);
-    });
+  entities.forEach((element) => {
+    element.draw(ctx);
+  });
 }
 
-function drawWall(ctx,x,y) {
-    var bs = blockSize/3;
-    ctx.fillStyle = "rgb(0,0,200)";
-    ctx.fillRect(x*blockSize+bs,y*blockSize+bs,bs,bs);
-    if ((x!=map[y].length-1) && (map[y][x+1]==0)) {
-        ctx.fillRect(x*blockSize+bs*2,y*blockSize+bs*1,bs,bs);
-    }
-    if ((x!=0) && (map[y][x-1]==0)) {
-        ctx.fillRect(x*blockSize+bs*0,y*blockSize+bs*1,bs,bs);
-    }
-    if ((y!=map.length-1) && (map[y+1][x]==0)) {
-        ctx.fillRect(x*blockSize+bs*1,y*blockSize+bs*2,bs,bs);
-    }
-    if ((y!=0) && (map[y-1][x]==0)) {
-        ctx.fillRect(x*blockSize+bs*1,y*blockSize+bs*0,bs,bs);
-    }
+function drawWall(ctx, x, y) {
+  var bs = blockSize / 3;
+  ctx.fillStyle = "rgb(0,0,200)";
+  ctx.fillRect(x * blockSize + bs, y * blockSize + bs, bs, bs);
+  if (x != map[y].length - 1 && map[y][x + 1] == 0) {
+    ctx.fillRect(x * blockSize + bs * 2, y * blockSize + bs * 1, bs, bs);
+  }
+  if (x != 0 && map[y][x - 1] == 0) {
+    ctx.fillRect(x * blockSize + bs * 0, y * blockSize + bs * 1, bs, bs);
+  }
+  if (y != map.length - 1 && map[y + 1][x] == 0) {
+    ctx.fillRect(x * blockSize + bs * 1, y * blockSize + bs * 2, bs, bs);
+  }
+  if (y != 0 && map[y - 1][x] == 0) {
+    ctx.fillRect(x * blockSize + bs * 1, y * blockSize + bs * 0, bs, bs);
+  }
 }
 
-function drawDoor(ctx,x,y) {
-    var bs = blockSize/3;
-    ctx.fillStyle = "rgb(200,200,00)";
-    ctx.fillRect(x*blockSize,y*blockSize+bs,blockSize,bs);
+function drawDoor(ctx, x, y) {
+  var bs = blockSize / 3;
+  ctx.fillStyle = "rgb(200,200,00)";
+  ctx.fillRect(x * blockSize, y * blockSize + bs, blockSize, bs);
 }
 
-function drawPoint(ctx,x,y) {
-    ctx.fillStyle = "rgb(255,255,255)";
-    ctx.beginPath();
-    ctx.arc(x*blockSize+blockSize/2, 
-    y*blockSize+blockSize/2, blockSize/8, 0, 2 * Math.PI);
-    ctx.fill(); 
+function drawPoint(ctx, x, y) {
+  ctx.fillStyle = "rgb(255,255,255)";
+  ctx.beginPath();
+  ctx.arc(
+    x * blockSize + blockSize / 2,
+    y * blockSize + blockSize / 2,
+    blockSize / 8,
+    0,
+    2 * Math.PI
+  );
+  ctx.fill();
 }
 
-function drawPowerUp(ctx,x,y) {
-    ctx.fillStyle = "rgb(255,255,255)";
-    ctx.beginPath();
-    ctx.arc(x*blockSize+blockSize/2, 
-    y*blockSize+blockSize/2, blockSize/3, 0, 2 * Math.PI);
-    ctx.fill(); 
+function drawPowerUp(ctx, x, y) {
+  ctx.fillStyle = "rgb(255,255,255)";
+  ctx.beginPath();
+  ctx.arc(
+    x * blockSize + blockSize / 2,
+    y * blockSize + blockSize / 2,
+    blockSize / 3,
+    0,
+    2 * Math.PI
+  );
+  ctx.fill();
 }
 
 function drawMap(ctx) {
-    for(var y=0;y<map.length;y++) {
-        for(var x=0;x<28;x++) {
-            switch (map[y][x]) {
-                case 0:
-                drawWall(ctx,x,y);
-                break;
-                case 1:
-                drawPoint(ctx,x,y);
-                break;
-                case 2:
-                drawPowerUp(ctx,x,y);
-                break;  
-                case 4:
-                drawDoor(ctx,x,y);
-                break;  
-                default:
-                break;
-            }
-        }
+  for (var y = 0; y < map.length; y++) {
+    for (var x = 0; x < 28; x++) {
+      switch (map[y][x]) {
+        case 0:
+          drawWall(ctx, x, y);
+          break;
+        case 1:
+          if (pendingDots[y][x]) {
+            drawPoint(ctx, x, y);
+          }
+          break;
+        case 2:
+          if (pendingDots[y][x]) {
+            drawPowerUp(ctx, x, y);
+          }
+          break;
+        case 4:
+          drawDoor(ctx, x, y);
+          break;
+        default:
+          break;
+      }
     }
+  }
 }
 
-function checkKey(e){
-    if(!e.repeat){
-        console.log("+")
-        player.checkKey(e);
+function checkKey(e) {
+  if (!e.repeat) {
+    console.log("+");
+    player.checkKey(e);
+  }
+}
+
+function clearKey() {
+  console.log("-");
+  player.clearKey();
+}
+
+function init() {
+  points = 0;
+  pendingDots = [];
+  for (var y = 0; y < map.length; y++) {
+    pendingDots.push([]);
+    for (var x = 0; x < 28; x++) {
+      if (map[y][x] == Map.DOT || map[y][x] == Map.POWER) {
+        pendingDots[y].push(true);
+      } else {
+        pendingDots[y].push(false);
+      }
     }
+  }
 }
 
-function clearKey(){
-    console.log("-")
-    player.clearKey();
+function checkEndGame() {
+  const [row, col] = player.getPosition();
+  if (pendingDots[row][col]) {
+    points++;
+    pendingDots[row][col] = false;
+  }
+
+  let count = 0;
+  pendingDots.forEach((row) => {
+    row.forEach((dot) => {
+      if (dot) count++;
+    });
+  });
+  if (count == 0) clearInterval(gameTimer);
 }
 
+var gameTimer;
 function game() {
-    setInterval(gameLoop, 1000/30);
-    document.addEventListener("keydown", checkKey);
-    document.addEventListener("keyup", clearKey);
+  init();
+  gameTimer = setInterval(gameLoop, 1000 / 30);
+  document.addEventListener("keydown", checkKey);
+  document.addEventListener("keyup", clearKey);
 }
